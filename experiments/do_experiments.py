780c0484e7d464b7abcba60cc559d691a2b4c7a8
gluckzhang <zhanglong3030@qq.com>
2021-06-03 16:49:30 +0200

    automatically query stats of peer info

--- experiments/do_experiments.py
+++ experiments/do_experiments.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Filename: do_experiments.py
 
-import os, sys, datetime, time, json, re, subprocess, signal, random
+import os, sys, requests, datetime, time, json, re, subprocess, signal, random, numpy
 import argparse, configparser
 import logging
 
@@ -52,6 +52,41 @@ def tail_client_log(client_log, timeout):
 
     return output
 
+def query_peer_stats(client_name, query_url, last_n_seconds):
+    end_ts = int(time.time())
+    start_ts = end_ts - last_n_seconds
+    response = requests.get(query_url.format(start=start_ts, end=end_ts))
+    results = None
+
+    if client_name == "openethereum":
+        status = response.json()["status"]
+        if status == "error":
+            logging.error("peer stats query failed")
+            logging.error(response.json())
+        else:
+            if len(response.json()['data']['result']) == 0:
+                logging.warning("peer stats query result is empty")
+            else:
+                results = response.json()['data']['result'][0]
+    elif client_name == "geth":
+        if "results" not in response.json():
+            logging.error("peer stats query failed")
+            logging.error(response.json())
+        else:
+            logging.info(response.json())
+            results = response.json()["results"][0]["series"][0]
+
+    # calculate statistic information of the values
+    if results != None:
+        values = numpy.array(results["values"]).astype(int)
+        min_value = numpy.percentile(values, 5, axis=0)[1] # in the values array, index 0: timestamp, index 1: failure rate
+        mean_value = numpy.mean(values, axis=0)[1]
+        max_value = numpy.percentile(values, 95, axis=0)[1]
+        variance = numpy.var(values, axis=0)[1]
+        results["stat"] = {"min": min_value, "mean": mean_value, "max": max_value, "variance": variance}
+
+    return results
+
 def dump_logs(content, filepath, filename):
     try:
         os.makedirs(filepath)
@@ -60,7 +95,15 @@ def dump_logs(content, filepath, filename):
     with open(os.path.join(filepath, filename), 'wt') as log_file:
         log_file.write(content.encode("utf-8"))
 
-def do_experiment(experiment, injector_path, client_name, client_log, dump_logs_path):
+def dump_metric(content, filepath, filename):
+    try:
+        os.makedirs(filepath)
+    except:
+        pass
+    with open(os.path.join(filepath, filename), "wt") as output:
+        json.dump(content, output, indent = 2)
+
+def do_experiment(experiment, injector_path, client_name, client_log, dump_logs_path, peer_stats_url):
     global INJECTOR
 
     # experiment principle
@@ -79,13 +122,17 @@ def do_experiment(experiment, injector_path, client_name, client_log, dump_logs_
 
     dump_logs_folder = "%s/%s%s-%s"%(dump_logs_path, experiment["syscall_name"], experiment["error_code"], experiment["failure_rate"])
 
+    result = dict()
     # step 1: 1 min normal execution, tail the log
     logging.info("1 min normal execution begins")
     normal_execution_log = tail_client_log(client_log, 60)
     dump_logs(normal_execution_log, dump_logs_folder, "normal.log")
+    normal_execution_peer_stat = query_peer_stats(client_name, peer_stats_url, 60)
+    dump_metric(normal_execution_peer_stat, dump_logs_folder, "normal_peer_stat.json")
+    result["peer_stat"] = dict()
+    result["peer_stat"]["normal"] = normal_execution_peer_stat["stat"]
 
     # step 2: error injection experiment
-    result = dict()
     # start the injector
     logging.info("%d seconds chaos engineering experiment begins"%experiment["experiment_duration"])
     INJECTOR = subprocess.Popen("python -u %s -p %s -P %s --errorno=%s %s"%(
@@ -105,6 +152,9 @@ def do_experiment(experiment, injector_path, client_name, client_log, dump_logs_
         logging.warning(injector_stdout.decode("utf-8"))
         logging.warning(injector_stderr.decode("utf-8"))
     dump_logs(ce_execution_log, dump_logs_folder, "ce.log")
+    ce_execution_peer_stat = query_peer_stats(client_name, peer_stats_url, experiment["experiment_duration"])
+    dump_metric(ce_execution_peer_stat, dump_logs_folder, "ce_peer_stat.json")
+    result["peer_stat"]["ce"] = ce_execution_peer_stat["stat"]
 
     # check if the chaos engineering experiment breaks the client
     pid = pgrep_the_client(client_name)
@@ -120,6 +170,9 @@ def do_experiment(experiment, injector_path, client_name, client_log, dump_logs_
         logging.info("5 min recovery phase observation begins")
         recovery_phase_log = tail_client_log(client_log, 60*5)
         dump_logs(recovery_phase_log, dump_logs_folder, "recovery.log")
+        recovery_phase_peer_stat = query_peer_stats(client_name, peer_stats_url, 60*5)
+        dump_metric(recovery_phase_peer_stat, dump_logs_folder, "recovery_peer_stat.json")
+        result["peer_stat"]["recovery"] = recovery_phase_peer_stat["stat"]
 
     logging.info(result)
     experiment["result"] = result
@@ -139,12 +192,13 @@ def main(config):
     client_path = config["EthClient"]["client_path"]
     restart_cmd = config["EthClient"]["restart_cmd"]
     client_log = config["EthClient"]["client_log"]
+    peer_stats_url = config["EthClient"]["peer_stats_url"]
 
     with open(error_models, 'rt') as file:
         experiments = json.load(file)
 
         for experiment in experiments["experiments"]:
-            experiment = do_experiment(experiment, syscall_injector, client_name, client_log, dump_logs_path)
+            experiment = do_experiment(experiment, syscall_injector, client_name, client_log, dump_logs_path, peer_stats_url)
             save_experiment_result(experiments, "%s-results.json"%client_name)
             if experiment["result"]["client_crashed"]:
                 new_pid = restart_client(client_name, client_path, restart_cmd, client_log)
